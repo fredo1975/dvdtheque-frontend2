886ceb6d2da006f869fea29398680bd2fcf77539
fredo <fred@autogrif.com>
2019-01-16 00:01:57 +0100

    add acteur adding to update film page

--- src/app/film-detail/film-detail.component.ts
+++ src/app/film-detail/film-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  Input } from '@angular/core';
+import { Component, OnInit,  Input, ViewChild } from '@angular/core';
 import { Film } from '../film';
 import { Personne } from '../personne';
 import { FilmService } from '../film.service';
@@ -11,16 +11,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class FilmDetailComponent implements OnInit {
   // @Input() film: Film;
+  @ViewChild('select') selectElRef;
   private film: Film;
   private realisateurs: Personne[];
+  private acteurs: Personne[];
+  private acteursFilm: Personne[];
   private annees: number[];
   private zonesList: number[];
-  constructor(private filmService: FilmService, private route: ActivatedRoute, private router: Router) { }
+  private newActeur: Personne;
+  private newActeurSetTemp: Personne[];
+  private newActeurSet: Personne[];
+  constructor(private filmService: FilmService, private route: ActivatedRoute, private router: Router) {
+    this.newActeur = new Personne(0, '' , '');
+  }
 
   ngOnInit() {
     this.filmService.getFilm(this.route.snapshot.params['id']).subscribe(_film => this.film = _film
       , (error) => {console.log('an error occured when fetching film with id : ' + this.route.snapshot.params['id']); });
     this.filmService.getAllPersonnes().subscribe((data: Personne[]) => {this.realisateurs = data; }
+      , (error) => {console.log('an error occured when fetching all personnes'); });
+      this.filmService.getAllPersonnes().subscribe((data: Personne[]) => {this.acteurs = data; }
       , (error) => {console.log('an error occured when fetching all personnes'); });
     this.annees = this.getAnneesSelect();
     this.zonesList = this.getZonesList();
@@ -44,5 +54,31 @@ export class FilmDetailComponent implements OnInit {
     }
     return zonesList;
   }
+  comparePersonne(a: Personne, b: Personne): boolean {
+    return a.id === b.id;
+  }
+  setSelected(selectElement) {
+    if (selectElement.checked === true) {
+      console.log('selectElement.checked ' + selectElement.value);
+    } else {
+      console.log('selectElement.unchecked');
+    }
+  }
 
+  addNewActeur() {
+    console.log('newActeur=' + this.newActeur.prenom + ' ' + this.newActeur.nom);
+    if (this.newActeurSetTemp === undefined) {
+      this.newActeurSetTemp = [];
+    }
+    const newAct = Object.assign({}, this.newActeur);
+    this.newActeurSetTemp = [...this.newActeurSetTemp , newAct];
+  }
+  updateFilm() {
+    if (this.newActeurSetTemp !== undefined) {
+      this.newActeurSet = [];
+    }
+    for (const act of this.newActeurSetTemp) {
+      console.log('act=' + act.prenom + ' ' + act.nom);
+    }
+  }
 }
