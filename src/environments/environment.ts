fbcf50c550011d77abc77f7f03c78effaabe1c75
fredo <fred@autogrif.com>
2019-03-24 21:05:18 +0100

    add rip date

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.100:8083/dvdtheque'
-  // apiUrl: 'http://localhost:8083/dvdtheque'
+  // apiUrl: 'http://192.168.1.100:8083/dvdtheque'
+  apiUrl: 'http://localhost:8083/dvdtheque'
 };
 
 /*
