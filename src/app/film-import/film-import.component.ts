b6e0d4946b1c9f5fa82bd47a0980554a198da220
fredo1975 <fredo@BLUECHIPIT-1>
2019-09-28 01:40:50 +0200

    finish import monitoring

--- src/app/film-import/film-import.component.ts
+++ src/app/film-import/film-import.component.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
 import { MessagingService } from './../messaging.service';
 import { StompState } from '@stomp/ng2-stompjs';
 import { JmsStatusMessage } from '../jms-status-message';
-
+import { JmsStatus } from '../jms-status.enum';
 @Component({
   selector: 'app-film-import',
   templateUrl: './film-import.component.html',
@@ -38,13 +38,11 @@ export class FilmImportComponent implements OnInit, OnDestroy {
     this.messagingService.disconnect();
   }
   importFilmList() {
-    console.log('importFilmList');
     this.buttonDisabled = true;
     this.loading = true;
     this.loadingStatus = true;
     this.messageHistory = [];
     this.filmService.importFilmList(this.formdata).subscribe((data: any) => {
-      console.log(data);
     }
       , (error) => {
         console.log(error);
@@ -61,21 +59,28 @@ export class FilmImportComponent implements OnInit, OnDestroy {
   private subscribeTopic() {
     // Subscribe to its stream (to listen on messages)
     this.messagingService.stream().subscribe((message: Message) => {
-      console.log('JSON.parse(message.body)=', JSON.parse(message.body));
       const jmsStatusMessage: JmsStatusMessage<any> = JmsStatusMessage.fromJson(JSON.parse(message.body));
-      console.log('jmsStatusFilm=', jmsStatusMessage);
-      this.messageHistory.unshift(jmsStatusMessage);
+      if (JmsStatus[jmsStatusMessage.getStatus()].toString() === JmsStatus.FILE_ITEM_READER_COMPLETED.toString()) {
+        this.messageHistory.splice(1, 1);
+        this.messageHistory.splice(1, 0, jmsStatusMessage);
+      } else if (JmsStatus[jmsStatusMessage.getStatus()].toString() === JmsStatus.FILM_CSV_LINE_MAPPER_COMPLETED.toString()) {
+        this.messageHistory.splice(2, 1);
+        this.messageHistory.splice(2, 0, jmsStatusMessage);
+      } else {
+        if (jmsStatusMessage.getStatusValue() === 1) {
+          this.messageHistory.shift();
+        }
+        this.messageHistory.unshift(jmsStatusMessage);
+      }
       this.loadingStatus = true;
     });
   }
 
   loadFile(event) {
-    console.log('loadFile event', event);
+    // console.log('loadFile event', event);
     const inputEl: HTMLInputElement = this.inputEl.nativeElement;
     const fileCount: number = inputEl.files.length;
-    console.log('loadFile event', fileCount);
     if (fileCount === 1) {
-      console.log('loadFile inputEl.files.item(i)=', inputEl.files.item(0));
       this.formdata = new FormData();
       this.formdata.append('file', inputEl.files.item(0));
     }
@@ -86,7 +91,7 @@ export class FilmImportComponent implements OnInit, OnDestroy {
     // Subscribe to its state (to know its connected or not)
     this.messagingService.state().subscribe((state: StompState) => {
       this.state = StompState[state];
-      console.log('this.state=', this.state);
+      // console.log('this.state=', this.state);
     });
   }
 }
--- src/app/jms-status-message.ts
+++ src/app/jms-status-message.ts
@@ -4,13 +4,20 @@ export class JmsStatusMessage<T> {
     private film: T;
     private status: JmsStatus;
     private timing;
-    constructor(_status: JmsStatus, _film: T, _timing) {
+    private statusValue;
+    constructor(_status: JmsStatus, _film: T, _timing, statusValue) {
         this.film = _film;
         this.status = _status;
         this.timing = _timing;
+        this.statusValue = statusValue;
     }
-
     public static fromJson(json: Object): JmsStatusMessage<any> {
-        return new JmsStatusMessage(json['status'], json['film'], json['timing']);
+        return new JmsStatusMessage(json['status'], json['film'], json['timing'], json['statusValue']);
+    }
+    public getStatusValue() {
+        return this.statusValue;
+    }
+    public getStatus(): JmsStatus {
+        return this.status;
     }
 }
