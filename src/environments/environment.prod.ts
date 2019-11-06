718bbed451e79bdcc1ff69d43d972a37d43b84d0
fredo1975 <fredo@BLUECHIPIT-1>
2019-11-06 21:54:33 +0100

    change apiurl to point to api-gateway for prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.100:8083/dvdtheque',
-  websocketApiUrl: 'ws://192.168.1.100:8083/dvdtheque/websocket'
+  apiUrl: 'http://192.168.1.106:8762/dvdtheque',
+  websocketApiUrl: 'ws://192.168.1.106:8762/dvdtheque/websocket'
 };
