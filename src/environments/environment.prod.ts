c0b7b7ffbef5a13b1b888a5862baba8702b686b1
fredo <fred@autogrif.com>
2021-11-21 01:24:51 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   authApi: 'http://192.168.1.105:8080/api/auth/',
   apiUrl: 'http://192.168.1.106:8762/dvdtheque',
   websocketApiUrl: 'ws://192.168.1.106:8762/dvdtheque/websocket',
-  configFile: 'assets/config/config.json'
+  configFile: 'assets/config/config.prod.json'
 };
