87ecb933089070ee4a3f01588179d14af9fbf23e
fredo <fred@autogrif.com>
2022-01-18 00:50:25 +0100

    add prod conf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   authApi: 'http://192.168.1.105:8080/api/auth/',
   apiUrl: 'http://192.168.1.106:8762/dvdtheque-service',
-  websocketApiUrl: 'ws://192.168.1.106:8762/dvdtheque-service/websocket',
+  websocketApiUrl: 'ws://192.168.1.106:8762/dvdtheque-ws/websocket',
   configFile: 'assets/config/config.prod.json'
 };
