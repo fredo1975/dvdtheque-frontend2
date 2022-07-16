97689450612367575c12e5c5ab4d95f5a17390bb
fredo <fred@autogrif.com>
2022-07-16 11:48:51 +0200

    enhancements

--- src/app/film-search-display/film-search-display.component.ts
+++ src/app/film-search-display/film-search-display.component.ts
@@ -37,56 +37,47 @@ export class FilmSearchDisplayComponent implements OnInit {
     this.displayTypes = [FilmDisplayType[FilmDisplayType.DERNIERS_AJOUTS], FilmDisplayType[FilmDisplayType.TOUS], FilmDisplayType[FilmDisplayType.DERNIERS_AJOUTS_NON_VUS]];
     this.filmSearch = new FilmSearch('', 0, false, null, null, null, false, Origine.TOUS, FilmDisplayType.TOUS);
     this.filmListParam = { realisateurs: [], acteurs: [], films: [], genres: [], realisateursLength: 0, acteursLength: 0 };
-    /*
-    this.loading = true;
-    forkJoin({
-      realisateurs: this.filmService.getAllRealisateursByOrigine(this.origine, this.displayType),
-      acteurs: this.filmService.getAllActeursByOrigine(this.origine, this.displayType),
-      genres: this.filmService.getAllGenres()
-    })
-    .subscribe(({realisateurs, acteurs, genres}) => {
-      this.filmListParam.realisateurs = [...realisateurs];
-      this.filmListParam.realisateursLength = this.filmListParam.realisateurs.length;
-      this.filmListParam.acteurs = [...acteurs];
-      this.filmListParam.acteursLength = this.filmListParam.acteurs.length;
-      this.filmListParam.genres = [...genres];
-    }, (error) => {
-      this.errorOccured = true;
-      this.loading = false;
-      console.log(error);
-    }
-    , () => {
-      this.loading = false;
-    });*/
   }
 
-  search(){
-    console.log(this.filmSearch.titre);
-    let query;
-    if(this.filmSearch.titre){
-      query = 'titre:eq:'+this.filmSearch.titre+':AND,';
+  search() {
+    let query = '';
+    if (this.filmSearch.titre) {
+      query = query.concat('titre:eq:' + this.filmSearch.titre + ':AND,');
     }
-    if(this.filmSearch.realisateur){
-      query = 'realisateur:eq:'+this.filmSearch.realisateur+':AND,';
+    if (this.filmSearch.realisateur) {
+      query = query.concat('realisateur:eq:' + this.filmSearch.realisateur + ':AND,');
     }
-    if(this.filmSearch.acteur){
-      query = 'acteur:eq:'+this.filmSearch.acteur+':AND,';
+    if (this.filmSearch.acteur) {
+      query = query.concat('acteur:eq:' + this.filmSearch.acteur + ':AND,');
     }
-    if(this.filmSearch.annee){
-      query = 'annee:eq:'+this.filmSearch.annee+':AND,';
+    if (this.filmSearch.annee) {
+      query = query.concat('annee:eq:' + this.filmSearch.annee + ':AND,');
     }
-    this.loading = true;
-    this.filmService.search(query,1,10,'-titre').subscribe((data: Film[]) => {
-      console.log(data);
-      this.films = [...data];
-    }
-      , (error) => {
-        this.errorOccured = true;
-        this.loading = false;
-        console.log(error);
+
+    if (query) {
+      //console.log(query);
+      this.loading = true;
+      this.filmService.search(query, 1, 10, '-titre').subscribe((data: Film[]) => {
+        console.log(data);
+        this.films = [...data];
       }
-      , () => {
-        this.loading = false;
-      });
+        , (error) => {
+          this.errorOccured = true;
+          this.loading = false;
+          console.log(error);
+        }
+        , () => {
+          this.loading = false;
+        });
+    }
+
+  }
+
+  resetFilter() {
+    this.filmSearch.titre = '';
+    this.filmSearch.realisateur = null;
+    this.filmSearch.acteur = null;
+    this.filmSearch.annee = null;
+    this.films = null;
   }
 }
